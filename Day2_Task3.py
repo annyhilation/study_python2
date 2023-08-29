4dd2907de04df6b9cf11ab59304c2aea313058e6
Anna <142794678+annyhilation@users.noreply.github.com>
2023-08-29 15:16:57 +0300

    Add files via upload

--- Day2_Task3.py
+++ Day2_Task3.py
@@ -1,17 +1,18 @@
 class Queue(list):
-    def front(self,lst):
+    def front(self):
+        super().__init__(lst)
         e_null = lst[0]
         self.lst = lst.pop(0)
         return f'Ушел из очереди: {e_null}'
 
-    def get_info(self, lst):
+    def get_info(self):
         return f'В очереди {len(lst)} человек: {", ".join(map(str,lst))}. Следующий в очереди: {lst[0]}'
 
 
 n = int(input('Введите количество человек в очереди: '))
 lst=[input('Введите имя человека в очереди: ') for i in range (0, n)]
 
-q = Queue(lst)
-print(q.front(lst),q.front(lst), sep = '\n')
-print(q.get_info(lst))
+q = Queue()
+print(q.front(),q.front(), sep = '\n')
+print(q.get_info())
 
